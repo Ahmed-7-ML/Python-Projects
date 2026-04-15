c64bff63fccaaab0465531ca4a2f60d6aee29c11
Ahmed Akram Amer | The Triple AI‬‏ <AHMED.Akram521@el-eng.menofia.edu.eg>
2026-04-15 17:44:33 +0200

    Update Convert Video to GIF.py

--- Convert Video to GIF/Convert Video to GIF.py
+++ Convert Video to GIF/Convert Video to GIF.py
@@ -1,6 +1,19 @@
 # from moviepy.editor import VideoFileClip
+
 from moviepy import VideoFileClip
+
+
 
 clip = VideoFileClip("sample.mp4")
 
-clip.write_gif("sample.gif", fps=30)
+
+
+clip.write_gif("sample.gif", fps=10)
+
+# you can play with it by changing the Number of frames per second (fps) [Frame Rate]
+
+
+
+# Increasing FPS -> Increasing the Size -> Make the Animiation GIF Smoother
+
+# Decreasing FPS -> Decreasing the Size -> Make the Animiation GIF Laggy
